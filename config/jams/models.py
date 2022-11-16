35993f44ca21d6154d336a4d8f26bf4cfaf796ee
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-16 20:39:16 +0000

    Added Song model

--- config/jams/models.py
+++ config/jams/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    duration = models.FloatField(null=False, default=.01, validators=[MinValueValidator(.01)])
+    num_plays = models.BigIntegerField(default=0, validators=[MinValueValidator(0)])
+    explicit = models.BooleanField(required=False)
