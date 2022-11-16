7cc52895297411c94c138e4ac98ac6dca13217a0
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-16 22:45:32 +0000

    Added one to many relationships through a newly created thru model or pivot table called ArtistSong

--- config/jams/models.py
+++ config/jams/models.py
@@ -7,7 +7,12 @@ class Song(models.Model):
     num_plays = models.BigIntegerField(default=0, validators=[MinValueValidator(0)])
     explicit = models.BooleanField()
 
+
 class Artist(models.Model):
     name = models.CharField(max_length=50,null=False, blank=False, unique=True)
     bio = models.TextField(max_length=500, null=False, blank=False, unique=True)
+
+class ArtistSong(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
     
