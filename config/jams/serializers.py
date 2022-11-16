0a730797c4a4c45dbb9cfc4146349d140672d278
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-16 22:18:17 +0000

    Finalized songs crud functionality

--- config/jams/serializers.py
+++ config/jams/serializers.py
@@ -3,7 +3,6 @@ from .models import Song
 from pprint import pprint
 
 class SongSerializer(serializers.ModelSerializer):
-    song = SongSerializer()
     class Meta:
         model = Song
         fields = "__all__"
