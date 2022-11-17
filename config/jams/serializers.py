ae1f6678634c0c38fca303326e5daac9c5c51fb2
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-17 17:30:12 +0000

    Added genre serializer and pivot table serializer

--- config/jams/serializers.py
+++ config/jams/serializers.py
@@ -12,12 +12,18 @@ class AlbumSerializer(serializers.ModelSerializer):
         model = Album
         fields = ('name',)
 
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name',)
+
 class SongSerializer(serializers.ModelSerializer):
     artists = ArtistSerializer(many=True)
     albums = AlbumSerializer(many=True)
+    genres = GenreSerializer(many=True)
     class Meta:
         model = Song
-        fields = ('artists', 'title', 'duration', 'num_plays', 'explicit', 'albums',)
+        fields = ('artists', 'title', 'duration', 'num_plays', 'genres', 'explicit', 'albums',)
 
     def create(self, validated_data):
         artist = validated_data.pop('artist')
@@ -33,4 +39,9 @@ class ArtistSongSerializer(serializers.ModelSerializer):
 class AlbumSongSerializer(serializers.ModelSerializer):
     class Meta:
         model = AlbumSong
+        fields = "__all__"
+
+class GenreSongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GenreSong
         fields = "__all__"
