7f35d6dc2ee5b5f230da3d9f9e6fdcef9a553722
smcconaha <110992120+smcconaha@users.noreply.github.com>
2022-11-18 09:01:00 -0500

    Update urls.py
    Added comment on album list router

--- config/jams/urls.py
+++ config/jams/urls.py
@@ -7,7 +7,7 @@ router.register(r'songs', SongViewSet)
 router.register(r'artist', ArtistViewSet)
 router.register(r'artist-songs', ArtistSongViewSet)
 router.register(r'album', AlbumViewSet)
-router.register(r'albumlist', AlbumlistViewSet)
+router.register(r'albumlist', AlbumlistViewSet) #this router may not funcition properly
 router.register(r'album-songs', AlbumSongViewSet)
 router.register(r'genre', AlbumViewSet)
 router.register(r'genre-songs', AlbumSongViewSet)
